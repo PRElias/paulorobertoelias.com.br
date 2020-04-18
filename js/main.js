84b5bc1dacd04010fa8af736fe225e3c744d9e44
PRElias <ppuspfc@gmail.com>
2020-04-18 20:54:23 -0300

    colocando o script no main

--- js/main.js
+++ js/main.js
@@ -138,3 +138,29 @@ var main = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
+
+
+if ('serviceWorker' in navigator) {
+  //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+  if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    //Register the ServiceWorker
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/pwabuilder-sw.js', {
+        scope: './'
+      }).then(function (reg) {
+        console.log('Service worker has been registered for scope:' + reg.scope);
+      });
+    });
+    var worker = new Worker('/pwabuilder-sw.js');
+
+    worker.addEventListener('message', function (e) {
+      console.log('Worker said: ', e.data);
+    }, false);
+
+    worker.postMessage('Worker doing work!');
+  }
+} else {
+  console.log("Navegador não aceita serviceWorker");
+}
